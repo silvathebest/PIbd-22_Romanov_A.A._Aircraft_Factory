1126f43cc446e2f70e0f9f77f635d1c737eb5b09
Alexey Romanov <51180322+silvathebest@users.noreply.github.com>
2021-04-22 01:57:30 +0400

    fix

--- AirCraftFactoryListImplement/Implements/OrderStorage.cs
+++ AirCraftFactoryListImplement/Implements/OrderStorage.cs
@@ -48,11 +48,13 @@ namespace AirCraftFactoryListImplement.Implements
             if (model == null) return null;
 
             List<OrderViewModel> result = new List<OrderViewModel>();
-            foreach (Order component in source.Orders)
+            foreach (Order order in source.Orders)
             {
-                if (component.DateCreate >= model.DateFrom && component.DateCreate <= model.DateTo)
+                if ((!model.DateFrom.HasValue && !model.DateTo.HasValue && order.DateCreate.Date == model.DateCreate.Date) ||
+                (model.DateFrom.HasValue && model.DateTo.HasValue && 
+                order.DateCreate.Date >= model.DateFrom.Value.Date && order.DateCreate.Date <= model.DateTo.Value.Date))
                 {
-                    result.Add(CreateModel(component));
+                    result.Add(CreateModel(order));
                 }
             }
             return result;
@@ -139,6 +141,7 @@ namespace AirCraftFactoryListImplement.Implements
                 Status = order.Status,
                 DateCreate = order.DateCreate,
                 DateImplement = order.DateImplement,
+                ClientId = order.ClientId,
                 ClientFIO = clientFio
             };
         }
